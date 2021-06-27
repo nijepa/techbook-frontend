82946f49d00fe5f0f48b87ace62c2fcbaaabff52
Nikola Pavicevic <nijepa@gmail.com>
2021-06-27 23:13:43 +0200

    login form

--- src/views/LogInUp.vue
+++ src/views/LogInUp.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="">
-    
-  </div>
+  <form class="">
+    <div>
+      <input id="email" name="email" type="email" required placeholder="email">
+      <label for="email">Your Name</label>
+    </div>
+    <div>
+      <input id="password" name="password" type="password" required placeholder="password">
+      <label for="password">Your Name</label>
+    </div>
+    <button type="submit" @click="logIn()">Log In</button>
+  </form>
 </template>
 
 <script>
